2da5e1edeeb31d340840ccc149384d345dbbe816
barnyster <mhmdnurakbar1928@gmail.com>
2026-02-25 10:45:34 +0700

    profile

--- resources/js/pages/profile/Show.vue
+++ resources/js/pages/profile/Show.vue
@@ -16,11 +16,11 @@ const props = defineProps({
       <hr />
 
       <div class="detail-card">
-        <p><b>Nama:</b> {{ user.name }}</p>
+        <p><b>Nama:</b> {{ user.nama }}</p>
         <p><b>Email:</b> {{ user.email }}</p>
         <p><b>Role:</b> {{ user.role }}</p>
-        <p><b>Status:</b> {{ user.status }}</p>
-        <p><b>Dibuat:</b> {{ user.created_at }}</p>
+        <!-- <p><b>Status:</b> {{ user.status }}</p> -->
+        <p><b>Dibuat:</b> {{ user.reg_times }}</p>
       </div>
         <Link href="/profile/edit" class="btn btn-primary" style="margin-left: 1050px;">Edit Profil</Link>
     </div>
